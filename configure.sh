6fc45db1aa42b98a2212b6e6f71f86b5b4977691
Christian Felipe Álvarez <sigilioso@gmail.com>
2019-08-04 12:58:04 +0200

    include configure and install-deps scripts

---
+++ configure.sh
@@ -0,0 +1,33 @@
+# TODO: install dependencies
+
+# TODO: doc in readme
+
+
+git submodule update --init --recursive
+
+source ./install-deps.sh
+
+echo "Installing fonts..."
+sh install-fonts.sh
+
+dotfiles_dir=`pwd`
+echo "Dotfiles dir: $dotfiles_dir"
+
+# link zsh / oh-my-zsh configuration
+echo "Setting up shell..."
+ln -sf $dotfiles_dir/shell/oh-my-zsh $HOME/.oh-my-zsh
+ln -sf $dotfiles_dir/shell/zshrc $HOME/.zshrc
+
+# link vim config
+echo "Setting up vim config..."
+ln -sf $dotfiles_dir/vim $HOME/.vim
+vim -c "PlugInstall"
+
+# .git configuration
+echo "Setting up git config..."
+ln -sf $dotfiles_dir/git/gitignore_global $HOME/.gitignore_global
+ln -sf $dotfiles_dir/git/gitconfig.static $HOME/.gitconfig.static
+git config --global include.path $HOME/.gitconfig.static
+echo 'Remember to setup user and email:'
+echo '    git config --global user.name "YOUR NAME"'
+echo '    git config --global user.email your@email'
---
+++ install-deps.sh
@@ -0,0 +1,46 @@
+
+install_zsh() {
+  # Test to see if zshell is installed.
+  if [ -z "$(command -v zsh)" ]; then
+    # If zsh isn't installed, get the platform of the current machine and
+    # install zsh with the appropriate package manager.
+    platform=$(uname);
+    if [[ $platform == 'Linux' ]]; then
+      if [[ -f /etc/redhat-release ]]; then
+        sudo yum install zsh
+      fi
+      if [[ -f /etc/debian_version ]]; then
+        sudo apt-get install zsh
+      fi
+    elif [[ $platform == 'Darwin' ]]; then
+      brew install zsh
+    fi
+  fi
+  # Set the default shell to zsh if it isn't currently set to zsh
+  if [[ ! "$SHELL" == "$(command -v zsh)" ]]; then
+    chsh -s "$(command -v zsh)"
+  fi
+}
+
+install_fzf() {
+    wget https://raw.githubusercontent.com/junegunn/fzf/master/install -O /tmp/install-fzf && chmod +x /tmp/install-fzf && /tmp/install-fzf
+}
+
+install_ag() {
+  # Test to see if zshell is installed.
+  if [ -z "$(command -v ag)" ]; then
+    # If zsh isn't installed, get the platform of the current machine and
+    # install zsh with the appropriate package manager.
+    platform=$(uname);
+    if [[ $platform == 'Linux' ]]; then
+      if [[ -f /etc/redhat-release ]]; then
+        sudo yum install the_silver_searcher
+      fi
+      if [[ -f /etc/debian_version ]]; then
+        sudo apt-get install silversearcher-ag
+      fi
+    elif [[ $platform == 'Darwin' ]]; then
+      brew install the_silver_searcher
+    fi
+  fi
+}
